6cf21541e96e7414cd531ed639df2bcf030011f1
Aravindan07 <aravindanravi33@gmail.com>
2022-07-25 11:16:06 +0530

    added redirect

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -44,6 +44,27 @@ onBackgroundMessage(messaging, (payload) => {
     .catch((error) => {
       console.log(error);
     });
+
+  self.addEventListener("notificationclick", function (event) {
+    let url = "https://example.com/some-path/";
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+      clients.matchAll({ type: "window" }).then((windowClients) => {
+        // Check if there is already a window/tab open with the target URL
+        for (var i = 0; i < windowClients.length; i++) {
+          var client = windowClients[i];
+          // If so, just focus it.
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
+        // If not, then open the target URL in a new window/tab.
+        if (clients.openWindow) {
+          return clients.openWindow(url);
+        }
+      })
+    );
+  });
 });
 
 onMessage(messaging, (payload) => {
@@ -63,4 +84,25 @@ onMessage(messaging, (payload) => {
     .catch((error) => {
       console.log(error);
     });
+
+  self.addEventListener("notificationclick", function (event) {
+    let url = "https://example.com/some-path/";
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+      clients.matchAll({ type: "window" }).then((windowClients) => {
+        // Check if there is already a window/tab open with the target URL
+        for (var i = 0; i < windowClients.length; i++) {
+          var client = windowClients[i];
+          // If so, just focus it.
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
+        // If not, then open the target URL in a new window/tab.
+        if (clients.openWindow) {
+          return clients.openWindow(url);
+        }
+      })
+    );
+  });
 });
